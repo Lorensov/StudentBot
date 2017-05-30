5dab0e6ae627a64044cb448f71afe1c155bae987
lorensov <lucianlorens@gmail.com>
2017-05-30 10:43:40 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import time
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+def set_up():
+  print("")
