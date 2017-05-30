42cd8b213067bc738751b66c83337c3c230d11ef
lorensov <lucianlorens@gmail.com>
2017-05-30 13:33:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,18 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
-def set_up():
-  print("")
+def set_up(url, browser="chrome"):
+  if (browser = "chrome"):
+    options = Options() 
+    options.add_argument("--start-maximized") 
+    driver = webdriver.Chrome(chrome_options=options)
+  
+  
+  elif (browser = "firefox"):
+    driver = webdriver.firefox()  
+  
+  driver.get(url)
+  return driver
+
+
+driver = set_up(url)
